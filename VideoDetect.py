6738248b26fadc59c59835ca50bd7a8006acb3b6
Kamikoto <Kamikoto-sama@users.noreply.github.com>
2018-07-28 13:44:13 +0700

    Face, eye and smile detection by image

--- VideoDetect.py
+++ VideoDetect.py
@@ -1,23 +1,23 @@
 import cv2
 
 def Detect_Faces(videoPath):
-	capture = cv2.VideoCapture(videoPath)
-	cascades = cv2.CascadeClassifier("faces.xml")
+    capture = cv2.VideoCapture(videoPath)
+    cascades = cv2.CascadeClassifier("faces.xml")
 
-	while(capture.isOpened()):
-	    ret, frame = capture.read()
+    while(capture.isOpened()):
+        ret, frame = capture.read()
 
-	    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	    faces = cascades.detectMultiScale(gray,scaleFactor = 1.1,
-	        minNeighbors = 5,minSize = (30,30))
+        faces = cascades.detectMultiScale(gray,scaleFactor = 1.1,
+            minNeighbors = 5,minSize = (30,30))
 
-	    for (x,y,w,h) in faces:
-	        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0))
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0))
 
-	    cv2.imshow('Press "q" to exit',frame)
-	    if cv2.waitKey(1) & 0xFF == ord('q'):
-	        break
+        cv2.imshow('Press "q" to exit',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-	capture.release()
-	cv2.destroyAllWindows()
+    capture.release()
+    cv2.destroyAllWindows()

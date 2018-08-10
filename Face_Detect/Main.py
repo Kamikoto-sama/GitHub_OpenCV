698d0208345e5cb7c8fea7d3fd03ad0b3e3cf457
Kamikoto <Kamikoto-sama@users.noreply.github.com>
2018-08-10 21:05:33 +0700

    Motion detect

--- Face_Detect/Main.py
+++ Face_Detect/Main.py
@@ -39,16 +39,16 @@ class Main(QtWidgets.QDialog):
 
     def switch_detect(self, status):
         if status:
-            self.detect.setText("Detect face")
+            self.detect.setText("Draw rectangles")
             self.detect_enabled = False
         else:
-            self.detect.setText("Set smile")
+            self.detect.setText("Draw img above")
             self.detect_enabled = True
 
     def start_cam(self):
         try:
-            self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
-            self.capture.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+            self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+            self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
             self.timer.start(5)
         except Exception as e:print(e)
 
@@ -61,45 +61,45 @@ class Main(QtWidgets.QDialog):
 
     def detect_face(self,img):
         try:
-            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             #gray = cv2.GaussianBlur(gray,(5,5),1)
 
-        # Detect faces
-            faces = face.detectMultiScale(gray,1.3,5,minSize = (30,30))
+            # Detect faces
+            faces = face.detectMultiScale(gray, 1.3,5, minSize = (30,30))
 
-        # Draw face rectangles
+            # Draw face rectangles
             if self.detect_enabled:
                 for (x,y,w,h) in faces:
-                    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+                    cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
                     _gray = gray[y:y+h, x:x+w]
                     _img = img[y:y+h, x:x+w]
 
-        #Detect smiles
+                    #Detect smiles
                     smiles = smile.detectMultiScale(_gray, 1.5, 25,
                                                          minSize=(25, 25))
-        #Draw smiles
+                    #Draw smiles
                     for (x, y, w, h) in smiles:
                         cv2.rectangle(
                           _img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        #Detect eyes
+                    #Detect eyes
                     eyes = eye.detectMultiScale(_gray, 1.5, 15,
                                                  minSize=(5, 5))
                     for (x,y,w,h) in eyes:
                         cv2.rectangle(_img, (x, y), (x + w, y + h), 
                                         (0, 0, 255), 2)
-        #Convert image into QImage
+            #Convert image into QImage
             qformat = QtGui.QImage.Format_RGB888
-            outImage = QtGui.QImage(img,img.shape[1],img.shape[0],
-                                    img.strides[0],qformat)
+            outImage = QtGui.QImage(img,img.shape[1], img.shape[0],
+                                    img.strides[0] , qformat)
             outImage = outImage.rgbSwapped()
 
-        #Draw overlay
+            #Draw overlay
             if not self.detect_enabled:
                 for (x,y,w,h) in faces:
                     paint = QtGui.QPainter(outImage)
                     image = QtGui.QPixmap(overlay)
-                    image = image.scaled(w,h)
-                    paint.drawPixmap(x,y,image)
+                    image = image.scaled(w, h)
+                    paint.drawPixmap(x, y, image)
 
             return outImage
         except Exception as e: print(e)
